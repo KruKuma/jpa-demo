4488d378e4ffcc3081470b64e885b04966a9a3b0
Naphatsakorn Khotsombat <n.khotsombat@mycit.ie>
2022-09-23 10:15:39 +0200

    Remove contents from main

--- src/main/java/com/example/jpademo/Main.java
+++ src/main/java/com/example/jpademo/Main.java
@@ -1,31 +1,8 @@
 package com.example.jpademo;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import com.example.jpademo.model.Employee;
-
 public class Main {
 
     public static void main(String[] args) {
-        
-        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
-        
-        EntityManager entityManager = emfactory.createEntityManager( );
-        entityManager.getTransaction( ).begin( );
-        
-        Employee employee = new Employee( ); 
-        employee.setEid( 1201 );
-        employee.setEname( "Gopal" );
-        employee.setSalary( 40000 );
-        employee.setDeg( "Technical Manager" );
-        
-        entityManager.persist( employee );
-        entityManager.getTransaction( ).commit( );
-
-        entityManager.close( );
-        emfactory.close( );
 
     }
 
