8ea75cfc0c66e22b20c7b6feca32cc5118ecd98d
Naphatsakorn Khotsombat <n.khotsombat@mycit.ie>
2022-09-23 10:30:34 +0200

    Add function for find employee

---
+++ src/main/java/com/example/jpademo/controller/FindEmployee.java
@@ -0,0 +1,26 @@
+package com.example.jpademo.controller;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.example.jpademo.model.Employee;
+
+public class FindEmployee {
+
+    public static void main(String[] args) {
+        
+EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Eclipselink_JPA" );
+        
+        EntityManager entityManager = emfactory.createEntityManager( );
+        entityManager.getTransaction( ).begin( );
+        Employee employee = entityManager.find(Employee.class, 1201);
+        
+        System.out.println("Employee ID = " + employee.getEid());
+        System.out.println("Employee Name = " + employee.getEname());
+        System.out.println("Employee Salary = " + employee.getSalary());
+        System.out.println("Employee Designation = " + employee.getDeg());
+
+    }
+
+}
